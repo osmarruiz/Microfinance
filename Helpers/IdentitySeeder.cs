4bdb0abcc40a7addc66672ed522feea560ff9dcc
osmarruiz <osmarernesto17@gmail.com>
2025-04-29 11:18:05 -0600

    Add IdentitySeeder for role creation and integrate into Program.cs

---
+++ Helpers/IdentitySeeder.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Microfinance.Helpers;
+
+public static class IdentitySeeder
+{
+    public static async Task CrearRoles(IServiceProvider serviceProvider)
+    {
+        var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+        string[] roles = { "Administrador", "Vendedor", "Cobrador" };
+
+        foreach (var role in roles)
+        {
+            if (!await roleManager.RoleExistsAsync(role))
+            {
+                await roleManager.CreateAsync(new IdentityRole(role));
+            }
+        }
+    }
+}
